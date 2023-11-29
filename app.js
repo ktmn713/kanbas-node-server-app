e41a6a6b7d45e0f00e81f91f4f70dce196da68af
ktmn713 <116754776+ktmn713@users.noreply.github.com>
2023-11-29 18:27:53 -0500

    a5 REST fa23

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import Hello from './hello.js';
 import Lab5 from './Lab5.js';
+import CourseRoutes from './courses/routes.js';
+import ModuleRoutes from './modules/routes.js';
 import cors from "cors";
 import "dotenv/config";
-import session from "express-session";
+// import session from "express-session";
 
 const app = express()
 app.use(cors());
+app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app)
 Hello(app)
 Lab5(app)
-app.listen(process.env.PORT || 4000);
+app.listen(4000);

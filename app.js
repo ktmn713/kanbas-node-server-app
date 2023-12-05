1eb3b2ae6658b88ad6456e32cd20501131abb8a7
ktmn713 <116754776+ktmn713@users.noreply.github.com>
2023-12-04 22:15:07 -0500

    a6 Mongo fa23

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ import CourseRoutes from './courses/routes.js';
 import ModuleRoutes from './modules/routes.js';
 import cors from "cors";
 import "dotenv/config";
+import mongoose from 'mongoose';
+import UserRoutes from './users/routes.js';
 // import session from "express-session";
 
+
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express()
 app.use(cors());
 app.use(express.json());
+
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app)
 Hello(app)
